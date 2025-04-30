06b0ff205572cf95b7873166eabd91fd1768baab
Pradipta Samanta <samanta@dkrz.de>
2025-04-30 16:18:28 +0200

    added option to find netcdf-c++4 libraries as well

--- cpp/icon-driver/cmake/FindnetCDF-CXX4.cmake
+++ cpp/icon-driver/cmake/FindnetCDF-CXX4.cmake
@@ -5,8 +5,9 @@ find_path(netCDF-CXX4_INCLUDE_DIR netcdf
   PATH_SUFFIXES include
 )
 
+# Search for either naming convention: netcdf-cxx4 (Homebrew/CMake) or netcdf_c++4 (MacPorts/Autotools)
 find_library(netCDF-CXX4_LIBRARY
-  NAMES netcdf-cxx4
+  NAMES netcdf-cxx4 netcdf_c++4
   HINTS
     ENV NETCDFCXX_DIR
   PATH_SUFFIXES lib lib64
@@ -20,6 +21,15 @@ find_package_handle_standard_args(netCDF-CXX4
 if(netCDF-CXX4_FOUND)
   set(netCDF-CXX4_LIBRARIES ${netCDF-CXX4_LIBRARY})
   set(netCDF-CXX4_INCLUDE_DIRS ${netCDF-CXX4_INCLUDE_DIR})
+
+  # Create an imported target for easier usage
+  if(NOT TARGET netCDF::CXX4)
+    add_library(netCDF::CXX4 UNKNOWN IMPORTED)
+    set_target_properties(netCDF::CXX4 PROPERTIES
+      IMPORTED_LOCATION "${netCDF-CXX4_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${netCDF-CXX4_INCLUDE_DIR}"
+    )
+  endif()
 endif()
 
 mark_as_advanced(netCDF-CXX4_INCLUDE_DIR netCDF-CXX4_LIBRARY)
